47e85c24d0fe93680c5b69ae115243527c950d80
Aditi Acharya <aditiacharya2@gmail.com>
2019-02-03 08:17:42 +0545

    removing functions

--- js/ball.js
+++ js/ball.js
@@ -19,29 +19,12 @@ class Ball{
         this.ballElement.style.transition= "transform 2s"; 
         this.ballElement.style.background ="url(images/bird.png)";
         this.ballElement.style.backgroundSize = "Cover"; 
-        this.ballUp = this.ballUp.bind(this);
-        this.ballDown = this.ballDown.bind(this);
+        // this.ballUp = this.ballUp.bind(this);
+        // this.ballDown = this.ballDown.bind(this);
         container.appendChild(this.ballElement);    
 
     }  
-    ballUp(){      
-        this.gravity = 0;  
-        this.yVelocity = - this.up;
-    }
-    ballDown(){       
-        this.gravity += 0.1;
-        this.topPos += this.gravity + this.yVelocity; 
-        this.ballElement.style.top = this.topPos + 'px';
-        if(this.topPos <= this.upperGap ){
-            this.yVelocity = -this.yVelocity; 
-             game.gameOver();
-      
-        }
-        if( this.topPos >= this.lowerGap ){
-            this.ballElement.style.top = this.lowerGap + 'px';  
-            game.gameOver();   
-        }           
-    }    
+       
 }
 
 
