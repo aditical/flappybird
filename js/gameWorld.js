328e44b0f3eb9ff6de20c1d0a4f7eedc69d9935e
Prashant-Acharya <pacharya97.pa@gmail.com>
2019-03-08 19:23:48 +0545

    Fixes the issue with restart button.

--- js/gameWorld.js
+++ js/gameWorld.js
@@ -1,133 +1,133 @@
 class Game{
-    constructor(){
-        this.gameElement = document.getElementsByClassName("container")[0];  
-        this.height = 500;
-        this.width = 900;
-        this.gameElement.style.height = this.height + 'px';
-        this.gameElement.style.width = this.width + 'px'; 
-        this.velocity = 1; 
-        this.buttonHeight = 40; 
-        this.buttonWidth = 100; 
-        this.buttonLeft = 400;
-        this.buttonTop = 250;   
-        this.obstacles = []; 
-        this.initialization();
-        this.isGameOver = false;
-        this.ballUp = this.ballUp.bind(this);
-        this.ballDown = this.ballDown.bind(this);
-        this.scoreCard = document.createElement('h1');
-        this.scoreCard.style.position = "absolute";
-        this.scoreCard.style.left = 700 + 'px';     
-        this.scoreCard.style.top = 10 + 'px'; 
-        this.scoreCard.style.zIndex = "2";
-        this.scoreCard.style.color = "#095864";
-        this.scoreCard.style.fontFamily =" Verdana";
-        this.scoreCard.style.fontSize = 50 + 'px';
-        this.gameElement.appendChild(this.scoreCard);
-        this.myMusic = new sound("sounds/mymus.mp3");
-        this.scoreMusic = new sound("sounds/score.mp3");  
-        this.gameOverMusic = new sound("sounds/gameo.mp3"); 
-         
-    }
-    initialization(){       
-        this.ball = new Ball(this.gameElement);   
-        this.backgroundElement = document.getElementsByClassName("background")[0];
-        this.background = new Background(this.backgroundElement);
-     }  
-    ballUp(){   
-        this.ball.gravity = 0;  
-        this.ball.yVelocity = - this.ball.up;
-    }
-    ballDown(){    
-        this.ball.gravity += 0.1;
-        this.ball.topPos += this.ball.gravity + this.ball.yVelocity; 
-        this.ball.ballElement.style.top = this.ball.topPos + 'px';
-        if(this.ball.topPos <= this.ball.upperGap ){
-            this.ball.yVelocity = -this.ball.yVelocity; 
-             this.gameOver();
-      
-        }
-        if( this.ball.topPos >= this.ball.lowerGap ){
-            this.ball.ballElement.style.top = this.ball.lowerGap + 'px';  
-            this.gameOver();   
-        }           
-    }       
-    moveBall(){   
-        this.move = setInterval(this.ballDown, 20) ;     
-        this.gameElement.addEventListener('mousedown',  this.ballUp);         
-      //  console.log(this.ball.leftPos );        
-    } 
-    checkCollision(ball, obstacle){
-        if(ball.leftPos + ball.width  > obstacle.leftPos && ball.leftPos < obstacle.leftPos + obstacle.width) {
-           if(ball.topPos <= obstacle.mintopPos + obstacle.y1 || ball.topPos + ball.height > obstacle.top1){               
-            this.ball.ballElement.style.zIndex = "2"; 
-            this.gameOver();
-           }                            
-        }                   
-    }
+	constructor(){
+		this.gameElement = document.getElementsByClassName("container")[0];  
+		this.height = 500;
+		this.width = 900;
+		this.gameElement.style.height = this.height + 'px';
+		this.gameElement.style.width = this.width + 'px'; 
+		this.velocity = 1; 
+		this.buttonHeight = 40; 
+		this.buttonWidth = 100; 
+		this.buttonLeft = 400;
+		this.buttonTop = 250;   
+		this.obstacles = []; 
+		this.initialization();
+		this.isGameOver = false;
+		this.ballUp = this.ballUp.bind(this);
+		this.ballDown = this.ballDown.bind(this);
+		this.scoreCard = document.createElement('h1');
+		this.scoreCard.style.position = "absolute";
+		this.scoreCard.style.left = 700 + 'px';     
+		this.scoreCard.style.top = 10 + 'px'; 
+		this.scoreCard.style.zIndex = "2";
+		this.scoreCard.style.color = "#095864";
+		this.scoreCard.style.fontFamily =" Verdana";
+		this.scoreCard.style.fontSize = 50 + 'px';
+		this.gameElement.appendChild(this.scoreCard);
+		this.myMusic = new sound("sounds/mymus.mp3");
+		this.scoreMusic = new sound("sounds/score.mp3");  
+		this.gameOverMusic = new sound("sounds/gameo.mp3"); 
+		 
+	}
+	initialization(){       
+		this.ball = new Ball(this.gameElement);   
+		this.backgroundElement = document.getElementsByClassName("background")[0];
+		this.background = new Background(this.backgroundElement);
+	 }  
+	ballUp(){   
+		this.ball.gravity = 0;  
+		this.ball.yVelocity = - this.ball.up;
+	}
+	ballDown(){    
+		this.ball.gravity += 0.1;
+		this.ball.topPos += this.ball.gravity + this.ball.yVelocity; 
+		this.ball.ballElement.style.top = this.ball.topPos + 'px';
+		if(this.ball.topPos <= this.ball.upperGap ){
+			this.ball.yVelocity = -this.ball.yVelocity; 
+			 this.gameOver();
+	  
+		}
+		if( this.ball.topPos >= this.ball.lowerGap ){
+			this.ball.ballElement.style.top = this.ball.lowerGap + 'px';  
+			this.gameOver();   
+		}           
+	}       
+	moveBall(){   
+		this.move = setInterval(this.ballDown, 20) ;     
+		this.gameElement.addEventListener('mousedown',  this.ballUp);         
+	  //  console.log(this.ball.leftPos );        
+	} 
+	checkCollision(ball, obstacle){
+		if(ball.leftPos + ball.width  > obstacle.leftPos && ball.leftPos < obstacle.leftPos + obstacle.width) {
+		   if(ball.topPos <= obstacle.mintopPos + obstacle.y1 || ball.topPos + ball.height > obstacle.top1){               
+			this.ball.ballElement.style.zIndex = "2"; 
+			this.gameOver();
+		   }                            
+		}                   
+	}
 
-    gameLoop(restart){
-        this.restart = restart
-        this.buttonHeightmyMusic;        
-        let highscore = 0   
-        let score = 0;         
-        this.moveBackgorund = setInterval (this.background.moveBackground, 30) ;
-        var that = this;  
-        this.obstacleInterval = setInterval (() => {
-            this.obstacle = new Obstacle();                   
-            this.obstacle.obstacleInit(that.gameElement, that.ball); 
-            this.obstacles.push(this.obstacle);                       
-        }, 3000);  
-        this.gameInterval = setInterval(()=> {
-            this.myMusic.play();
-            this.scoreCard.innerHTML =  + score;
-            for(let i =0 ; i < this.obstacles.length; i++){
-             this.checkCollision(this.ball, this.obstacles[i]); 
-            // console.log(this.ball.leftPos , this.obstacles[i].leftPos)
-             if(this.ball.leftPos == this.obstacles[i].leftPos){
-                 score++;
-                 this.scoreMusic.play();
-             }           
-            }
-            if(highscore <= score){
-                highscore = score;
-                localStorage.setItem('highscore' , JSON.stringify(highscore)); 
-            }
-            if(score == 10){
-                this.ball.ballElement.style.background ="url(images/blue.png)";
-                this.ball.ballElement.style.backgroundSize = "Cover"; 
-            } else if (score == 20){
-                this.ball.ballElement.style.background ="url(images/green.png)";
-                this.ball.ballElement.style.backgroundSize = "Cover"; 
-            } else if(score >= 30) {
-                this.ball.ballElement.style.background ="url(images/pink.png)";
-                this.ball.ballElement.style.backgroundSize = "Cover"; 
-            }
-        }, 100) ;
-       }         
-       gameOver(){             
-           this.myMusic.stop(); 
-           this.gameElement.removeEventListener('mousedown',  this.ball.ballUp); 
-           clearInterval(this.moveBackgorund);
-           clearInterval(this.obstacleInterval);
-           clearInterval(this.gameInterval);
-           this.gameOverText  = document.createElement('h1');  
-           this.gameOverText.style.position = "absolute"; 
-           this.gameOverText.style.fontWeight = "bold";
-           this.gameOverText.style.color = "#095864";
-           this.gameOverText.style.fontFamily ="Verdana";            
-           this.gameOverText.style.fontSize = 40 + 'px';
-           this.gameOverText.style.left = 290 + 'px';     
-           this.gameOverText.style.top = 150 + 'px';
-           this.gameOverText.innerHTML = "GAME OVER";
-           this.gameElement.appendChild(this.gameOverText);
-           this.gameOverMusic.play();
-           //this.startButton.style.display = "block";
-           for(let i= 0; i < this.obstacles.length ; i++){
-            this.obstacles[i].stopObstacle();
-           }        
-           this.restart.style.display = "block";    
-    }    
+	gameLoop(restart){
+		this.restart = restart
+		this.buttonHeightmyMusic;        
+		let highscore = 0   
+		let score = 0;         
+		this.moveBackgorund = setInterval (this.background.moveBackground, 30) ;
+		var that = this;  
+		this.obstacleInterval = setInterval (() => {
+			this.obstacle = new Obstacle();                   
+			this.obstacle.obstacleInit(that.gameElement, that.ball); 
+			this.obstacles.push(this.obstacle);                       
+		}, 3000);  
+		this.gameInterval = setInterval(()=> {
+			this.myMusic.play();
+			this.scoreCard.innerHTML =  + score;
+			for(let i =0 ; i < this.obstacles.length; i++){
+			 this.checkCollision(this.ball, this.obstacles[i]); 
+			// console.log(this.ball.leftPos , this.obstacles[i].leftPos)
+			 if(this.ball.leftPos == this.obstacles[i].leftPos){
+				 score++;
+				 this.scoreMusic.play();
+			 }           
+			}
+			if(highscore <= score){
+				highscore = score;
+				localStorage.setItem('highscore' , JSON.stringify(highscore)); 
+			}
+			if(score == 10){
+				this.ball.ballElement.style.background ="url(images/blue.png)";
+				this.ball.ballElement.style.backgroundSize = "Cover"; 
+			} else if (score == 20){
+				this.ball.ballElement.style.background ="url(images/green.png)";
+				this.ball.ballElement.style.backgroundSize = "Cover"; 
+			} else if(score >= 30) {
+				this.ball.ballElement.style.background ="url(images/pink.png)";
+				this.ball.ballElement.style.backgroundSize = "Cover"; 
+			}
+		}, 100) ;
+	   }         
+	   gameOver(){             
+		   this.myMusic.stop(); 
+		   this.gameElement.removeEventListener('mousedown',  this.ball.ballUp); 
+		   clearInterval(this.moveBackgorund);
+		   clearInterval(this.obstacleInterval);
+		   clearInterval(this.gameInterval);
+		   this.gameOverText  = document.createElement('h1');  
+		   this.gameOverText.style.position = "absolute"; 
+		   this.gameOverText.style.fontWeight = "bold";
+		   this.gameOverText.style.color = "#095864";
+		   this.gameOverText.style.fontFamily ="Verdana";            
+		   this.gameOverText.style.fontSize = 40 + 'px';
+		   this.gameOverText.style.left = 290 + 'px';     
+		   this.gameOverText.style.top = 150 + 'px';
+		   this.gameOverText.innerHTML = "GAME OVER";
+		   this.gameElement.appendChild(this.gameOverText);
+		   this.gameOverMusic.play();
+		   //this.startButton.style.display = "block";
+		   for(let i= 0; i < this.obstacles.length ; i++){
+			this.obstacles[i].stopObstacle();
+		   }        
+		   this.restart.style.display = "block";    
+	}    
 }
 let game = new Game();
 var startButton = document.createElement("button");
@@ -143,7 +143,7 @@ restartButton.style.fontWeight = "bold";
 restartButton.style.color = "black";
 restartButton.style.fontFamily ="monospace"; 
 restartButton.onclick = () => {
-    window.location.reload();       
+	window.location.reload();       
 }
 game.gameElement.appendChild(restartButton);
 restartButton.style.display = "none";
@@ -157,12 +157,13 @@ startButton.style.backgroundColor = "#efb300";
 startButton.innerHTML = "START GAME";
 startButton.style.fontWeight = "bold";
 startButton.style.color = "black";
-startButton.style.fontFamily ="monospace"; 
+startButton.style.fontFamily ="monospace";
+startButton.style.zIndex = 1 
 startButton.onclick = () => {
-    startButton.style.display ="none";   
-    instructionButton.style.display= "none";  
-    game.moveBall();
-    game.gameLoop(restartButton);
+	startButton.style.display ="none";   
+	instructionButton.style.display= "none";  
+	game.moveBall();
+	game.gameLoop(restartButton);
 }
 game.gameElement.appendChild(startButton);
 instructionButton.style.height =  40 + 'px';
@@ -207,18 +208,18 @@ instruction.style.fontSize = 17 + 'px';
 instruction.style.color = "black";
 instruction.style.fontFamily ="monospace"; 
 instructionButton.onclick = () => {
-    startButton.style.display ="none";  
-    instructionButton.style.display= "none";  
-    instruction.style.display ="block";
-    backButton.style.display= "block";
-    game.gameElement.appendChild(instruction);
-    game.gameElement.appendChild(backButton);
+	startButton.style.display ="none";  
+	instructionButton.style.display= "none";  
+	instruction.style.display ="block";
+	backButton.style.display= "block";
+	game.gameElement.appendChild(instruction);
+	game.gameElement.appendChild(backButton);
 }
 backButton.onclick = () => {
-    startButton.style.display ="block";  
-    instructionButton.style.display= "block"; 
-    instruction.style.display = "none";
-    backButton.style.display= "none";
+	startButton.style.display ="block";  
+	instructionButton.style.display= "block"; 
+	instruction.style.display = "none";
+	backButton.style.display= "none";
 }
 
 
